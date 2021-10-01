acdd22eb134ec6b9c7fd8e857d2e6e0e827001d6
Vineet Tiwari <vineettiwari864@gmail.com>
2021-10-01 15:41:42 +0530

    frontend userAuth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Switch , Route} from 'react-router';
+import {BrowserRouter} from 'react-router-dom'
 import './App.css';
+import Header from './Header';
+import LoginUser from './LoginUser';
+import RegistrationForm from './RegistrationForm';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Header/>
+     <Switch>
+     <Route exact path='/' component={RegistrationForm} />
+     <Route exact path='/login'component={LoginUser} />
+      </Switch>
+  </BrowserRouter>
   );
 }
 
